514a3ba623316422dde5b87e90d5e1908e714e5e
gopsur1975 <gopsur1975@gmail.com>
2016-02-14 23:12:39 +0530

    Calculate Tax

--- app/scripts/modules/calculate-tax/calculate-tax.js
+++ app/scripts/modules/calculate-tax/calculate-tax.js
@@ -4,8 +4,15 @@ angular.module('TaxCalculator')
 	this.years = [];
 	var currentDate = new Date();
 	var currentYear = currentDate.getFullYear();
-	
+	this.taxableIncome = 0;
+    this.totalTax = 0;
 	for(var i=0;i<3;i++){
 		this.years.push((currentYear-i)+'-'+(currentYear-(i-1)));
 	}
+    this.AY = _.first(this.years);
+    
+    this.calculateTax=function(){
+        this.totalTax=20;
+    }
+    
 });
